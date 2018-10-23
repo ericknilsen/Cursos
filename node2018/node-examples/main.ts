f3f5b6ca699a4d6fcdb2be82cbcd05d1e20f7fd3
ericknilsen <ericknilsen@gmail.com>
2018-10-23 10:07:59 -0300

    Curso de Node

---
+++ node2018/node-examples/main.ts
@@ -0,0 +1,8 @@
+import {fatorial} from './fatorial'
+
+console.log('n-fatorial')
+const argv = require('yargs').demandOption('num').argv
+
+const num = argv.num
+
+console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
