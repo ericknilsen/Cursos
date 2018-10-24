773e07d4f8f7e4d53843bb7816f842e76e5509be
ericknilsen <ericknilsen@gmail.com>
2018-10-24 12:32:45 -0300

    Curso de Node

--- node2018/meat-api/server/server.ts
+++ node2018/meat-api/server/server.ts
@@ -1,10 +1,20 @@
 import * as restify from 'restify'
+import * as mongoose from 'mongoose'
+
 import {environment} from '../common/environment'
 import {Router} from '../common/router'
+import {mergePatchBodyParser} from './merge-patch.parser'
 
 export class Server {
 
   application: restify.Server
+
+  initializeDb(): mongoose.MongooseThenable {
+    (<any>mongoose).Promise = global.Promise
+    return mongoose.connect(environment.db.url, {
+      useMongoClient: true
+    })
+  }
 
   initRoutes(routers: Router[]): Promise<any>{
     return new Promise((resolve, reject)=>{
@@ -16,10 +26,12 @@ export class Server {
         })
 
         this.application.use(restify.plugins.queryParser())
+        this.application.use(restify.plugins.bodyParser())
+        this.application.use(mergePatchBodyParser)
 
         //routes
         for (let router of routers) {
-          router.applyRoutes(this.application)  
+          router.applyRoutes(this.application)
         }
 
         this.application.listen(environment.server.port, ()=>{
@@ -33,7 +45,8 @@ export class Server {
   }
 
   bootstrap(routers: Router[] = []): Promise<Server>{
-      return this.initRoutes(routers).then(()=> this)
+      return this.initializeDb().then(()=>
+             this.initRoutes(routers).then(()=> this))
   }
 
 }
